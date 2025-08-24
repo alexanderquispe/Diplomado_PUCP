266d2e1236d1695a8c64c2c6ebdd15ef40f2da5a
Rossytalu <146303626+Rossytalu@users.noreply.github.com>
2025-08-23 21:19:48 -0500

    Update assignment2.py
    Parte 1 del ejercicio

--- Lecture_1/Assigment_1/group_10_assignment1_2025/assignment2.py
+++ Lecture_1/Assigment_1/group_10_assignment1_2025/assignment2.py
@@ -1,3 +1,16 @@
+## Parte 2
+# Declaramos una variable de cada tipo:
+entero = 10            #int
+decimal = 3.14         #float
+texto = "Buenos días"  #string
+booleano = True        #booleano
+
+# Mostramos las variables
+print(type(entero))   
+print(type(decimal))  
+print(type(texto))   
+print(type(booleano)) 
+
 import pandas as pd
 
 import numpy as np
